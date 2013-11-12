f71fb39b6b018597e2f50dc3bf179650a0fcd9e3
devon townsend <devonstownsend@gmail.com>
2013-11-11 22:15:49 -0800

    porting codeplex repository

---
+++ js/audio.js
@@ -0,0 +1,181 @@
+﻿
+
+
+function LoadAudio(src) {
+
+    //A hack to get around ioS audio loading restrictions
+    //On the first touch of the user, we start playing the track to load the file.
+    //Then we immediately pause the playback so no sound is heard
+    var userAgent = window.navigator.userAgent;
+    var click = document.onclick;
+    if (click === undefined) {
+        click = "touchstart";
+    }
+    else {
+        var click = "click";
+    }
+
+    var audio = document.createElement('audio');
+    audio.src = src;
+    audio.autobuffer = true;
+    audio.load();
+    audio.muted = false;
+    console.log("ua test 1: " + (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)));
+    if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
+        
+        $("#intro-modal").attr('style', 'display:inline');
+        var pauseAudio = function () {
+            audio.pause();
+            audio.removeEventListener('play', pauseAudio, false);
+        }
+
+        var startLoad = function () {
+            audio.play();
+            document.removeEventListener(click, startLoad, false);
+            $("#modal-loading-icon").attr('style', 'display:inline');
+            $(".modal-footer p").attr('style', 'display:none');
+        }
+
+        var dismissModal = function () {
+        $("#intro-modal").attr('style', 'display:none');
+        }
+
+        audio.addEventListener('play', pauseAudio, false);
+
+        $("#intro-modal").bind(click, function () {
+            startLoad();
+        });
+
+        audio.addEventListener('canplaythrough', dismissModal, false);
+    }
+
+    this.audio = audio;
+
+}
+
+
+var playSound = function (soundClip) {
+    //Each clip must be seperated by at least one second
+    try {
+        var spriteData = {
+            chaBoy: {
+                start: 0.0,
+                end: 1.0
+            },
+            buhDuhDuh: {
+                start: 2.0,
+                end: 3.2
+            },
+            itsAGrounder: {
+                start: 4,
+                end: 5
+            },
+            turnTwo: {
+                start: 6,
+                end: 7
+            },
+
+            youCrazyDevon: {
+                start: 8,
+                end: 9.4
+            },
+
+            codyDamnSon: {
+                start: 10,
+                end: 13
+            },
+
+            codyFuckWithYourBoys: {
+                start: 14,
+                end: 16
+            },
+
+            helloMonica: {
+                start: 17,
+                end: 19.4
+            },
+
+            damn: {
+                start: 20,
+                end: 20.4
+            },
+
+            turnDownForWhat: {
+                start: 21,
+                end: 22.4
+            },
+
+            trapWhatsHappening: {
+                start: 24,
+                end: 24.8
+            },
+
+            yourBoyJayz: {
+                start: 26,
+                end: 26.8
+            },
+
+            youCrazyJayz: {
+                start: 28,
+                end: 29.4
+            },
+
+            gunShot: {
+                start: 30,
+                end: 30.7
+            },
+
+            gunCock: {
+                start: 31.5,
+                end: 32.1
+            },
+
+            dickMarron: {
+                start: 33,
+                end: 35.4
+            },
+
+            woo: {
+                start: 36,
+                end: 38
+            },
+
+            hiJo: {
+                start: 39,
+                end: 40
+            },
+
+            helloMonicaAvi: {
+                start: 40.5,
+                end: 43.3
+            },
+
+            chea: {
+                start: 44,
+                end: 44.5
+            }
+        };
+        var audioFile = this.audio;
+
+        audioFile.currentTime = spriteData[soundClip].start;
+        audioFile.play();
+
+
+        var checkTime = function () {
+            if ((this.currentTime >= spriteData[soundClip].end) &&
+                (this.currentTime <= spriteData[soundClip].end + 0.5)) {
+                this.pause();
+                audioFile.removeEventListener('timeupdate', checkTime, false);
+            }
+        }
+
+        audioFile.addEventListener('timeupdate', checkTime, false);
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+LoadAudio('sounds/trapComp.mp3')
+
+
---
+++ js/scroll.js
@@ -0,0 +1,93 @@
+﻿
+$(document).ready(function(){
+	var userAgent = window.navigator.userAgent;
+	console.log(navigator.userAgent)
+	//if (detectmob()) {
+	if(true){
+		//get the tops of each section
+		var categoryTop = new Array();
+
+		var children = document.getElementsByClassName("category-label");
+		console.log(children);
+		for( var i = 0; i<children.length; i++){
+			categoryTop[i] = ($(children[i]).offset().top);
+			console.log(categoryTop[i]);
+		};
+
+
+		//if the screen resizes, get the tops again
+		$(window).resize(function(){
+			var children = document.getElementsByClassName("category-label");
+			console.log(children);
+			for( var i = 0; i<children.length; i++){
+				categoryTop[i] = ($(children[i]).offset().top);
+				console.log(categoryTop[i]);
+			};
+		});
+
+		var scrollStopTime = 200;
+		var scrollPadding = $(window).height() / 2;
+		var scrollAnimationTime = 200;
+
+	    $(window).bind('mousewheel', function (event) {
+		    clearTimeout($.data(this, 'scrollTimer'));
+		    $.data(this, 'scrollTimer', setTimeout(function() {
+	    	var body = $("body");
+	    	var screenTop = body.scrollTop();
+
+	    	//inside header
+	    	if(screenTop <= categoryTop[0]){
+	    		//if scroll down
+	    		if (event.originalEvent.wheelDelta <= 0) {
+	    			//jump to 2
+	    			body.animate({scrollTop:categoryTop[1]}, scrollAnimationTime);
+	    			console.log("jump to 2");
+	    		}
+	    		else{
+	    			//go to top
+	    			body.animate({scrollTop:0}, scrollAnimationTime);
+	    			console.log("jump to top");
+	    		}
+	    	}
+
+	    	//inside middle sections
+	    	else{
+		    	for(var i = 0; i < categoryTop.length -1; i++){
+			    	if(screenTop >= categoryTop[i]  - scrollPadding &&
+			    		screenTop < categoryTop[i + 1] - scrollPadding){
+
+			    		//if scroll up
+				    	if (event.originalEvent.wheelDelta >= 0) {
+				    		body.animate({scrollTop:categoryTop[i - 1]}, scrollAnimationTime);
+				            console.log("jump to " + (i));
+				        }
+
+				        //if scroll down
+				        else {
+				            body.animate({scrollTop:categoryTop[i + 1]}, scrollAnimationTime);
+				            console.log("b:jump to " + (i+1));
+				        }
+			    	}
+		    	}
+		    }
+
+	    	//inside last section
+	    	if(screenTop >= categoryTop[categoryTop.length - 1] - scrollPadding){
+
+	    		//if scroll up
+		    	if (event.originalEvent.wheelDelta >= 0) {
+		    		body.animate({scrollTop:categoryTop[categoryTop.length - 2]}, scrollAnimationTime);
+		            console.log("jump to 3");
+		        }
+		        else {
+		            body.animate({scrollTop:categoryTop[categoryTop.length - 1]}, scrollAnimationTime);
+					console.log("jump to end");
+		        }
+	    	}
+	    	}, scrollStopTime));
+	    	
+	    });
+			
+
+	}
+});
